f047a7940d4c5ccb0f968c295fc5e844f5329cdf
Ryan Lovelett <ryan@wahvee.com>
2012-06-16 09:57:45 -0500

    Installed Resque for processing background jobs.

--- Gemfile
+++ Gemfile
@@ -22,7 +22,7 @@ group :assets do
   gem 'uglifier', '>= 1.0.3'
 end
 
-
+gem 'resque', '~> 1.20.0', require: "resque/server"
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- config/routes.rb
+++ config/routes.rb
@@ -50,6 +50,9 @@ RpcDemo::Application.routes.draw do
   # just remember to delete public/index.html.
   root :to => 'welcome#index'
 
+  # Resque
+  mount Resque::Server, :at => "/resque"
+
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
