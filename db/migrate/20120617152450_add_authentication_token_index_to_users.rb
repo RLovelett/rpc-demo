c41bc4a9dcd0fbae56558ee8c6ebd0bc3029b100
Ryan Lovelett <ryan@wahvee.com>
2012-06-17 10:26:42 -0500

    Index the Authentication Token.
    Should help to optimize the finding of user tokens when validating they
    are logged into the system.

---
+++ db/migrate/20120617152450_add_authentication_token_index_to_users.rb
@@ -0,0 +1,5 @@
+class AddAuthenticationTokenIndexToUsers < ActiveRecord::Migration
+  def change
+    add_index  :users, :authentication_token, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120616224833) do
+ActiveRecord::Schema.define(:version => 20120617152450) do
 
   create_table "commands", :force => true do |t|
     t.string   "name"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(:version => 20120616224833) do
     t.string   "faye_client_id"
   end
 
+  add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["faye_client_id"], :name => "index_users_on_faye_client_id"
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
